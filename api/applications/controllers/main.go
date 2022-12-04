5334e7164256ebe3c19bdadddd2ed3bc28729827
MrEdvard <1638026+mredvard@users.noreply.github.com>
2022-12-04 21:09:24 +0800

    api: Add main script to run the api server

--- api/applications/controllers/main.go
+++ api/applications/controllers/main.go
@@ -6,18 +6,23 @@ import (
 	"github.com/gin-gonic/gin"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
 	"log"
 	"net/http"
+	"time"
 )
 
 func ListApplications(c *gin.Context) {
-	conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.Dial("localhost:50007", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	applicationClient := apps_pb.NewApplicationServiceClient(conn)
-	response, err := applicationClient.ListApplications(context.Background(), &apps_pb.ListApplicationsRequest{
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
+	defer cancel()
+	ctx = metadata.AppendToOutgoingContext(ctx, "dapr-app-id", "conure_apps_api")
+	response, err := applicationClient.ListApplications(ctx, &apps_pb.ListApplicationsRequest{
 		AccountId: 0,
 	})
 	if err != nil {
