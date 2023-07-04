558c85ea837fa20c4bfec8e197f219f1ae97a738
mredvard <1638026+mredvard@users.noreply.github.com>
2023-07-04 18:19:45 +0800

    feat: Add component type and update the manifests

--- api/oam/v1alpha1/application_types.go
+++ api/oam/v1alpha1/application_types.go
@@ -20,16 +20,9 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // ApplicationSpec defines the desired state of Application
 type ApplicationSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of Application. Edit application_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	Components []Component `json:"components"`
 }
 
 // ApplicationStatus defines the observed state of Application
