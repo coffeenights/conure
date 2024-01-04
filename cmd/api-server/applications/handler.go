38c4f980ed9c6fab57a4495271255f9aaa216af7
mredvard <1638026+mredvard@users.noreply.github.com>
2024-01-04 16:05:28 +0800

    feat: initialise the app at the handler and not at the router

--- cmd/api-server/applications/handler.go
+++ cmd/api-server/applications/handler.go
@@ -1,11 +1,25 @@
 package applications
 
-import "github.com/coffeenights/conure/cmd/api-server/database"
+import (
+	apiConfig "github.com/coffeenights/conure/cmd/api-server/config"
+	"github.com/coffeenights/conure/cmd/api-server/database"
+	"github.com/coffeenights/conure/internal/config"
+	"log"
+)
 
 type AppHandler struct {
 	MongoDB *database.MongoDB
+	Config  *apiConfig.Config
 }
 
-func NewAppHandler(mongoDB *database.MongoDB) *AppHandler {
-	return &AppHandler{MongoDB: mongoDB}
+func NewAppHandler() *AppHandler {
+	appConfig := config.LoadConfig(apiConfig.Config{})
+	mongo, err := database.ConnectToMongoDB(appConfig.MongoDBURI)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &AppHandler{
+		MongoDB: mongo,
+		Config:  appConfig,
+	}
 }
--- cmd/api-server/routes/groups.go
+++ cmd/api-server/routes/groups.go
@@ -2,18 +2,12 @@ package routes
 
 import (
 	apps "github.com/coffeenights/conure/cmd/api-server/applications"
-	"github.com/coffeenights/conure/cmd/api-server/database"
 	"github.com/gin-gonic/gin"
-	"log"
 )
 
 func GenerateRouter() *gin.Engine {
 	router := gin.Default()
-	handler, err := database.ConnectToMongoDB()
-	if err != nil {
-		log.Fatal(err)
-	}
-	app := apps.NewAppHandler(handler)
+	app := apps.NewAppHandler()
 	apps.GenerateRoutes("/applications", router, app)
 	return router
 }
