3160d1baaba6c0a89c129dfc3a33c7fe985e55e5
mredvard <1638026+mredvard@users.noreply.github.com>
2024-01-05 17:48:46 +0800

    feat: Create the softDelete method in the Organization model. And updated the GetById method and Update method to filter out the entries marked as deleted

--- cmd/api-server/applications/models.go
+++ cmd/api-server/applications/models.go
@@ -7,17 +7,25 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"log"
+	"time"
 )
 
 type Organization struct {
 	ID        primitive.ObjectID `bson:"_id,omitempty"`
-	Status    string             `bson:"status,omitempty"`
-	AccountId string             `bson:"accountId,omitempty"`
-	Name      string             `bson:"name,omitempty"`
+	Status    OrganizationStatus `bson:"status"`
+	AccountId string             `bson:"accountId"`
+	Name      string             `bson:"name"`
+	CreatedAt time.Time          `bson:"createdAt"`
+	DeletedAt time.Time          `bson:"deletedAt,omitempty"`
 }
 
+type OrganizationStatus string
+
 const (
-	OrganizationCollection = "organizations"
+	OrganizationCollection                    = "organizations"
+	OrgActive              OrganizationStatus = "active"
+	OrgDeleted             OrganizationStatus = "deleted"
+	OrgDisabled            OrganizationStatus = "disabled"
 )
 
 func (o *Organization) String() string {
@@ -26,6 +34,8 @@ func (o *Organization) String() string {
 
 func (o *Organization) Create(mongo *database.MongoDB) error {
 	collection := mongo.Client.Database(mongo.DBName).Collection(OrganizationCollection)
+	o.CreatedAt = time.Now()
+	o.Status = OrgActive
 	insertResult, err := collection.InsertOne(context.Background(), o)
 	if err != nil {
 		log.Fatal(err)
@@ -34,23 +44,25 @@ func (o *Organization) Create(mongo *database.MongoDB) error {
 	return nil
 }
 
-func (o *Organization) GetById(mongo *database.MongoDB, Id string) *Organization {
+func (o *Organization) GetById(mongo *database.MongoDB, Id string) (*Organization, error) {
 	collection := mongo.Client.Database(mongo.DBName).Collection(OrganizationCollection)
-	err := collection.FindOne(context.Background(), bson.M{"accountid": Id}).Decode(o)
+	filter := bson.M{"accountId": Id, "status": bson.M{"$ne": OrgDeleted}}
+	err := collection.FindOne(context.Background(), filter).Decode(o)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	log.Println("Found a single document: ", o)
-	return o
+	return o, nil
 }
 
 func (o *Organization) Update(mongo *database.MongoDB) error {
 	collection := mongo.Client.Database(mongo.DBName).Collection(OrganizationCollection)
-	filter := bson.D{{"accountid", o.AccountId}}
+	filter := bson.M{"accountId": o.AccountId, "status": bson.M{"$ne": OrgDeleted}}
 	update := bson.D{
 		{"$set", bson.D{
 			{"status", o.Status},
 			{"accountId", o.AccountId},
+			{"name", o.Name},
 		}},
 	}
 	updateResult, err := collection.UpdateOne(context.Background(), filter, update)
@@ -63,7 +75,7 @@ func (o *Organization) Update(mongo *database.MongoDB) error {
 
 func (o *Organization) Delete(mongo *database.MongoDB) error {
 	collection := mongo.Client.Database(mongo.DBName).Collection(OrganizationCollection)
-	filter := bson.D{{"accountid", o.AccountId}}
+	filter := bson.D{{"accountId", o.AccountId}}
 	deleteResult, err := collection.DeleteOne(context.Background(), filter)
 	if err != nil {
 		log.Fatal(err)
@@ -72,28 +84,19 @@ func (o *Organization) Delete(mongo *database.MongoDB) error {
 	return nil
 }
 
-//func (o *Organization) Migrate(client *mongo.Client) error {
-//	log.Println("Migrating MongoDB")
-//	coll := client.Database(mongo.DBName).Collection(OrganizationCollection)
-//	validator := bson.M{
-//		"$jsonSchema": bson.M{
-//			"bsonType": "object",
-//			"required": []string{"field1", "field2"},
-//			"properties": bson.M{
-//				"field1": bson.M{
-//					"bsonType":    "string",
-//					"description": "must be a string and is required",
-//				},
-//				"field2": bson.M{
-//					"bsonType":    "int",
-//					"description": "must be an integer and is required",
-//				},
-//			},
-//		},
-//	}
-//	opts := options.CreateCollection().SetValidator(validator)
-//	err := client.Database(mongo.DBName).RunCommand(context.Background(), bson.D{{"create", OrganizationCollection}, {"validator", validator}}).Err()
-//	if err != nil {
-//		// Handle error
-//	}
-//}
+func (o *Organization) SoftDelete(mongo *database.MongoDB) error {
+	collection := mongo.Client.Database(mongo.DBName).Collection(OrganizationCollection)
+	filter := bson.D{{"accountId", o.AccountId}}
+	update := bson.D{
+		{"$set", bson.D{
+			{"status", OrgDeleted},
+			{"deletedAt", time.Now()},
+		}},
+	}
+	updateResult, err := collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Matched %v documents and deleted %v documents.\n", updateResult.MatchedCount, updateResult.ModifiedCount)
+	return nil
+}
--- cmd/api-server/applications/models_test.go
+++ cmd/api-server/applications/models_test.go
@@ -1,20 +1,21 @@
 package applications
 
 import (
-	"context"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	apiConfig "github.com/coffeenights/conure/cmd/api-server/config"
+	"github.com/coffeenights/conure/cmd/api-server/database"
+	"github.com/coffeenights/conure/internal/config"
+	_ "github.com/joho/godotenv/autoload"
 	"testing"
 )
 
-func setupDB() (*mongo.Client, error) {
-	// Connect to your MongoDB
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-	client, err := mongo.Connect(context.Background(), clientOptions)
+func setupDB() (*database.MongoDB, error) {
+	appConfig := config.LoadConfig(apiConfig.Config{})
+	testDBName := appConfig.MongoDBName + "-test"
+	client, err := database.ConnectToMongoDB(appConfig.MongoDBURI, testDBName)
 	if err != nil {
 		return nil, err
 	}
-	return client, nil
+	return &database.MongoDB{Client: client.Client, DBName: testDBName}, nil
 }
 
 func TestOrganization_Create(t *testing.T) {
@@ -23,7 +24,7 @@ func TestOrganization_Create(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	org := &Organization{Status: "Active", AccountId: "12345"}
+	org := &Organization{Status: OrgActive, AccountId: "12345", Name: "Test Organization"}
 
 	err = org.Create(client)
 	if err != nil {
@@ -37,13 +38,13 @@ func TestOrganization_GetById(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	org := &Organization{Status: "Active", AccountId: "12345"}
+	org := &Organization{Status: OrgActive, AccountId: "12345"}
 	err = org.Create(client)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	got := org.GetById(client, org.AccountId)
+	got, err := org.GetById(client, org.AccountId)
 	if got.AccountId != org.AccountId {
 		t.Errorf("Got %v, want %v", got.AccountId, org.AccountId)
 	}
@@ -55,20 +56,66 @@ func TestOrganization_Update(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	org := &Organization{Status: "Active", AccountId: "12345"}
+	org := &Organization{Status: OrgActive, AccountId: "12345"}
 	err = org.Create(client)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	org.Status = "Inactive"
+	org.Status = OrgDisabled
 	err = org.Update(client)
 	if err != nil {
 		t.Errorf("Failed to update organization: %v", err)
 	}
 
-	got := org.GetById(client, org.AccountId)
-	if got.Status != "Inactive" {
-		t.Errorf("Got %v, want %v", got.Status, "Inactive")
+	got, err := org.GetById(client, org.AccountId)
+	if got.Status != OrgDisabled {
+		t.Errorf("Got %v, want %v", got.Status, OrgDisabled)
+	}
+}
+
+func TestOrganization_Delete(t *testing.T) {
+	client, err := setupDB()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	org := &Organization{Status: OrgActive, AccountId: "12345"}
+	err = org.Create(client)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = org.Delete(client)
+	if err != nil {
+		t.Errorf("Failed to delete organization: %v", err)
+	}
+
+	_, err = org.GetById(client, org.ID.Hex())
+	if err == nil {
+		t.Errorf("Got 1 document, want 0")
+	}
+}
+
+func TestOrganization_SoftDelete(t *testing.T) {
+	client, err := setupDB()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	org := &Organization{Status: OrgActive, AccountId: "12345"}
+	err = org.Create(client)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = org.SoftDelete(client)
+	if err != nil {
+		t.Errorf("Failed to soft delete organization: %v", err)
+	}
+
+	_, err = org.GetById(client, org.ID.Hex())
+	if err == nil {
+		t.Errorf("Got 1 document, want 0")
 	}
 }
