cc00a5ea5b1729099267de654c65f365febb63ab
mredvard <1638026+mredvard@users.noreply.github.com>
2024-01-08 18:24:08 +0800

    fix: Use proper httpStatus codes

--- cmd/api-server/applications/environment.go
+++ cmd/api-server/applications/environment.go
@@ -4,6 +4,7 @@ import (
 	k8sUtils "github.com/coffeenights/conure/internal/k8s"
 	"github.com/gin-gonic/gin"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"net/http"
 )
 
 func (a *AppHandler) GetOrganization(c *gin.Context) {
@@ -11,40 +12,40 @@ func (a *AppHandler) GetOrganization(c *gin.Context) {
 	org := Organization{}
 	_, err := org.GetById(a.MongoDB, organizationID)
 	if err != nil {
-		c.JSON(404, gin.H{})
+		c.JSON(http.StatusNotFound, gin.H{})
 		return
 	}
 	response := OrganizationResponse{}
 	response.ParseModelToResponse(&org)
-	c.JSON(200, response)
+	c.JSON(http.StatusOK, response)
 }
 
 func (a *AppHandler) CreateOrganization(c *gin.Context) {
 	org := Organization{}
 	err := c.BindJSON(&org)
 	if err != nil {
-		c.JSON(500, gin.H{
+		c.JSON(http.StatusInternalServerError, gin.H{
 			"error": err.Error(),
 		})
 		return
 	}
 	_, err = org.Create(a.MongoDB)
 	if err != nil {
-		c.JSON(500, gin.H{
+		c.JSON(http.StatusInternalServerError, gin.H{
 			"error": err.Error(),
 		})
 		return
 	}
 	response := OrganizationResponse{}
 	response.ParseModelToResponse(&org)
-	c.JSON(200, response)
+	c.JSON(http.StatusCreated, response)
 }
 
 func (a *AppHandler) ListEnvironments(c *gin.Context) {
 	// creates the clientset
 	genericClientset, err := k8sUtils.GetClientset()
 	if err != nil {
-		c.JSON(500, gin.H{
+		c.JSON(http.StatusInternalServerError, gin.H{
 			"error": err.Error(),
 		})
 		return
@@ -55,13 +56,13 @@ func (a *AppHandler) ListEnvironments(c *gin.Context) {
 	// get the k8s namespaces information
 	namespaces, err := genericClientset.K8s.CoreV1().Namespaces().List(c, labelSelector)
 	if err != nil {
-		c.JSON(500, gin.H{
+		c.JSON(http.StatusInternalServerError, gin.H{
 			"error": err.Error(),
 		})
 		return
 	}
 	// return the information to the client
-	c.JSON(200, gin.H{
+	c.JSON(http.StatusOK, gin.H{
 		"namespaces": namespaces,
 	})
 }
