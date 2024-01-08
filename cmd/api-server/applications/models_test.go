0ca517aab1abe6d18f4db73f598e43e1cfe9c57e
mredvard <1638026+mredvard@users.noreply.github.com>
2024-01-08 16:46:15 +0800

    tests: Move the setupDB Function to another file, and update the tests to the recent changes in the Create method

--- cmd/api-server/applications/models_test.go
+++ cmd/api-server/applications/models_test.go
@@ -1,22 +1,9 @@
 package applications
 
 import (
-	apiConfig "github.com/coffeenights/conure/cmd/api-server/config"
-	"github.com/coffeenights/conure/cmd/api-server/database"
-	"github.com/coffeenights/conure/internal/config"
 	_ "github.com/joho/godotenv/autoload"
 	"testing"
 )
-
-func setupDB() (*database.MongoDB, error) {
-	appConfig := config.LoadConfig(apiConfig.Config{})
-	testDBName := appConfig.MongoDBName + "-test"
-	client, err := database.ConnectToMongoDB(appConfig.MongoDBURI, testDBName)
-	if err != nil {
-		return nil, err
-	}
-	return &database.MongoDB{Client: client.Client, DBName: testDBName}, nil
-}
 
 func TestOrganization_Create(t *testing.T) {
 	client, err := setupDB()
@@ -26,7 +13,7 @@ func TestOrganization_Create(t *testing.T) {
 
 	org := &Organization{Status: OrgActive, AccountId: "12345", Name: "Test Organization"}
 
-	err = org.Create(client)
+	_, err = org.Create(client)
 	if err != nil {
 		t.Errorf("Failed to create organization: %v", err)
 	}
@@ -39,12 +26,12 @@ func TestOrganization_GetById(t *testing.T) {
 	}
 
 	org := &Organization{Status: OrgActive, AccountId: "12345"}
-	err = org.Create(client)
+	_, err = org.Create(client)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	got, err := org.GetById(client, org.AccountId)
+	got, _ := org.GetById(client, org.AccountId)
 	if got.AccountId != org.AccountId {
 		t.Errorf("Got %v, want %v", got.AccountId, org.AccountId)
 	}
@@ -57,7 +44,7 @@ func TestOrganization_Update(t *testing.T) {
 	}
 
 	org := &Organization{Status: OrgActive, AccountId: "12345"}
-	err = org.Create(client)
+	_, err = org.Create(client)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -81,7 +68,7 @@ func TestOrganization_Delete(t *testing.T) {
 	}
 
 	org := &Organization{Status: OrgActive, AccountId: "12345"}
-	err = org.Create(client)
+	_, err = org.Create(client)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -104,7 +91,7 @@ func TestOrganization_SoftDelete(t *testing.T) {
 	}
 
 	org := &Organization{Status: OrgActive, AccountId: "12345"}
-	err = org.Create(client)
+	_, err = org.Create(client)
 	if err != nil {
 		t.Fatal(err)
 	}
---
+++ cmd/api-server/applications/test_utils.go
@@ -0,0 +1,27 @@
+package applications
+
+import (
+	apiConfig "github.com/coffeenights/conure/cmd/api-server/config"
+	"github.com/coffeenights/conure/cmd/api-server/database"
+	"github.com/coffeenights/conure/internal/config"
+	"github.com/gin-gonic/gin"
+	_ "github.com/joho/godotenv/autoload"
+)
+
+func setupDB() (*database.MongoDB, error) {
+	appConfig := config.LoadConfig(apiConfig.Config{})
+	testDBName := appConfig.MongoDBName + "-test"
+	client, err := database.ConnectToMongoDB(appConfig.MongoDBURI, testDBName)
+	if err != nil {
+		return nil, err
+	}
+	return &database.MongoDB{Client: client.Client, DBName: testDBName}, nil
+}
+
+func setupRouter() (*gin.Engine, *AppHandler) {
+	router := gin.Default()
+	app := NewAppHandler()
+	app.MongoDB.DBName = app.MongoDB.DBName + "-test"
+	GenerateRoutes("/organizations", router, app)
+	return router, app
+}
