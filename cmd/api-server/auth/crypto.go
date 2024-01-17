3413b7f27e237023cd2689270cedc8fd023fb5e3
Oscar Lotero <oscarelotero@gmail.com>
2024-01-17 11:17:32 +0100

    feat: add the crypto functions to hash/un-hash the password and generate a JWT

---
+++ cmd/api-server/auth/crypto.go
@@ -0,0 +1,167 @@
+package auth
+
+import (
+	"crypto/rand"
+	"crypto/subtle"
+	"encoding/base64"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+	"golang.org/x/crypto/argon2"
+)
+
+const (
+	MEMORY      = 64 * 1024
+	ITERATIONS  = 3
+	PARALLELISM = 2
+	SALTLENGTH  = 16
+	KEYLENGTH   = 32
+)
+
+type Argon struct {
+	memory      uint32
+	iterations  uint32
+	parallelism uint8
+	saltLength  uint32
+	keyLength   uint32
+	salt        []byte
+	hash        []byte
+}
+
+type UserData struct {
+	Id    string `json:"id"`
+	Email string `json:"email"`
+}
+
+type JWTClaims struct {
+	Data UserData `json:"data"`
+	jwt.StandardClaims
+}
+
+func GenerateFromPassword(password string) (string, error) {
+
+	// Generate a cryptographically secure random salt.
+	salt, err := GenerateRandomBytes(SALTLENGTH)
+	if err != nil {
+		return "", err
+	}
+
+	// Generate a hash of the password using the Argon2 id variant.
+	hash := argon2.IDKey([]byte(password), salt, ITERATIONS, MEMORY, PARALLELISM, KEYLENGTH)
+
+	// Base64 encode the salt and hashed password.
+	b64Salt := base64.RawStdEncoding.EncodeToString(salt)
+	b64Hash := base64.RawStdEncoding.EncodeToString(hash)
+
+	// Return a string using the standard encoded hash representation.
+	encodedHash := fmt.Sprintf("$argon2id$v=%d$m=%d,t=%d,p=%d$%s$%s", argon2.Version, MEMORY, ITERATIONS,
+		PARALLELISM, b64Salt, b64Hash)
+
+	return encodedHash, nil
+}
+
+func ComparePasswordAndHash(password string, encodedHash string) (bool, error) {
+	// Extract the parameters, salt and derived key from the encoded password hash.
+	argon, err := decodeHash(encodedHash)
+	if err != nil {
+		return false, err
+	}
+
+	// Derive the key from the other password using the same parameters.
+	otherHash := argon2.IDKey([]byte(password), argon.salt, argon.iterations, argon.memory, argon.parallelism,
+		argon.keyLength)
+
+	// Check that the contents of the hashed passwords are identical
+	if subtle.ConstantTimeCompare(argon.hash, otherHash) == 1 {
+		return true, nil
+	}
+	return false, nil
+}
+
+func decodeHash(encodedHash string) (p *Argon, err error) {
+	values := strings.Split(encodedHash, "$")
+	if len(values) != 6 {
+		return nil, ErrCryptoHandler
+	}
+
+	var version int
+	_, err = fmt.Sscanf(values[2], "v=%d", &version)
+	if err != nil {
+		return nil, err
+	}
+	if version != argon2.Version {
+		return nil, ErrCryptoHandler
+	}
+
+	p = &Argon{}
+	_, err = fmt.Sscanf(values[3], "m=%d,t=%d,p=%d", &p.memory, &p.iterations, &p.parallelism)
+	if err != nil {
+		return nil, err
+	}
+
+	p.salt, err = base64.RawStdEncoding.Strict().DecodeString(values[4])
+	if err != nil {
+		return nil, err
+	}
+	p.saltLength = uint32(len(p.salt))
+
+	p.hash, err = base64.RawStdEncoding.Strict().DecodeString(values[5])
+	if err != nil {
+		return nil, err
+	}
+	p.keyLength = uint32(len(p.hash))
+
+	return p, nil
+}
+
+func GenerateRandomBytes(n uint32) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+func GenerateToken(ttl time.Duration, payload map[string]string, secretJWTKey string) (string, error) {
+	now := time.Now().UTC()
+
+	claims := JWTClaims{}
+	claims.Subject = payload["sub"]
+	claims.ExpiresAt = now.Add(ttl).Unix()
+	claims.IssuedAt = now.Unix()
+	claims.NotBefore = now.Unix()
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString([]byte(secretJWTKey))
+
+	if err != nil {
+		return "", fmt.Errorf("generating JWT Token failed: %w", err)
+	}
+
+	return tokenString, nil
+}
+
+func ValidateToken(tokenString string, secretJWTKey string) (JWTClaims, error) {
+	claims := JWTClaims{}
+
+	// Parse the JWT string and store the result in `claims`.
+	tokenObject, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(secretJWTKey), nil
+	})
+	if err != nil {
+		if err.Error() == jwt.ErrSignatureInvalid.Error() {
+			return claims, ErrTokenNotValid
+		}
+		return claims, err
+	}
+	if !tokenObject.Valid {
+		return claims, ErrTokenNotValid
+	}
+
+	return claims, nil
+}
---
+++ cmd/api-server/auth/models.go
@@ -0,0 +1,97 @@
+package auth
+
+import (
+	"context"
+	"log"
+	"regexp"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
+	"github.com/coffeenights/conure/cmd/api-server/database"
+)
+
+const UserCollection string = "users"
+
+type User struct {
+	ID          primitive.ObjectID `bson:"_id,omitempty"`
+	Email       string
+	Password    string
+	IsActive    bool
+	LastLoginAt *time.Time
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+func (u *User) Create(mongo *database.MongoDB) error {
+	collection := mongo.Client.Database(mongo.DBName).Collection(UserCollection)
+	u.CreatedAt = time.Now()
+	u.UpdatedAt = u.CreatedAt
+	u.IsActive = true
+
+	// check if email exists
+	filter := bson.M{"email": u.Email}
+	err := collection.FindOne(context.Background(), filter).Decode(u)
+	if err == nil {
+		return ErrEmailExists
+	}
+
+	insertResult, err := collection.InsertOne(context.Background(), u)
+	if err != nil {
+		return err
+	}
+	u.ID = insertResult.InsertedID.(primitive.ObjectID)
+	log.Println("Inserted a single document: ", u.ID.Hex())
+	return nil
+}
+
+func (u *User) GetById(mongo *database.MongoDB, id string) error {
+	collection := mongo.Client.Database(mongo.DBName).Collection(UserCollection)
+	IDHex, _ := primitive.ObjectIDFromHex(id)
+	filter := bson.M{"_id": IDHex, "isActive": true}
+	err := collection.FindOne(context.Background(), filter).Decode(u)
+	if err != nil {
+		return err
+	}
+	log.Println("Found a single document: ", u)
+	return nil
+}
+
+func (u *User) GetByEmail(mongo *database.MongoDB, email string) error {
+	collection := mongo.Client.Database(mongo.DBName).Collection(UserCollection)
+	filter := bson.M{"email": email, "isActive": true}
+	err := collection.FindOne(context.Background(), filter).Decode(u)
+	if err != nil {
+		return err
+	}
+	log.Println("Found a single document: ", u)
+	return nil
+}
+
+func ValidateEmail(email string) error {
+	pattern := `^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`
+	if matched, err := regexp.MatchString(pattern, email); err != nil {
+		return ErrEmailNotValid
+	} else if !matched {
+		return ErrEmailNotValid
+	}
+	return nil
+}
+
+func ValidatePassword(password string) error {
+	if len(password) < 8 {
+		return ErrPasswordNotValid
+	}
+	if !regexp.MustCompile(`[A-Z]`).MatchString(password) {
+		return ErrPasswordNotValid
+	}
+	if !regexp.MustCompile(`[a-z]`).MatchString(password) {
+		return ErrPasswordNotValid
+	}
+	if !regexp.MustCompile(`[0-9]`).MatchString(password) {
+		return ErrPasswordNotValid
+	}
+
+	return nil
+}
