2a2ebf314e3f83cf787709c78808f673bfb874e4
Oscar Lotero <oscarelotero@gmail.com>
2024-02-13 17:14:49 +0100

    feat: add login endpoint

--- cmd/api-server/auth/handler.go
+++ cmd/api-server/auth/handler.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 
@@ -22,7 +23,41 @@ func NewAuthHandler(config *apiConfig.Config, mongo *database.MongoDB) *Handler 
 }
 
 func (h *Handler) Login(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message": "Login"})
+	loginRequest := LoginRequest{}
+	err := c.ShouldBindJSON(&loginRequest)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user := User{}
+	err = user.GetByEmail(h.MongoDB, loginRequest.Email)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": ErrEmailPasswordValid.Error()})
+		return
+	}
+
+	matched, err := ComparePasswordAndHash(loginRequest.Password, user.Password)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	if !matched {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": ErrEmailPasswordValid.Error()})
+		return
+	}
+
+	payload := JWTData{
+		Email: user.Email,
+	}
+	ttl := time.Duration(h.Config.JWTExpiration) * time.Hour * 24
+	jwt, err := GenerateToken(ttl, payload, h.Config.JWTSecret)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"token": jwt})
 }
 
 func (h *Handler) Me(c *gin.Context) {
