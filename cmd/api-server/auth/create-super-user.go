450b10aceb4652c10b36b2c769d429a7b1c9cc3b
Oscar Lotero <oscarelotero@gmail.com>
2024-02-19 16:48:13 +0100

    feat: Add function to reset the superuser password

--- cmd/api-server/auth/create-super-user.go
+++ cmd/api-server/auth/create-super-user.go
@@ -8,7 +8,7 @@ import (
 	"github.com/coffeenights/conure/internal/config"
 )
 
-func CreateSuperUser() {
+func CreateSuperuser() {
 	conf := config.LoadConfig(apiConfig.Config{})
 	mongo, err := database.ConnectToMongoDB(conf.MongoDBURI, conf.MongoDBName)
 	if err != nil {
@@ -34,7 +34,43 @@ func CreateSuperUser() {
 	}
 
 	log.Println("x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x")
-	log.Println("Super user created")
+	log.Println("Superuser created")
+	log.Println("Email:", email)
+	log.Println("Password:", password)
+	log.Println("x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x")
+}
+
+func ResetSuperuserPassword() {
+	conf := config.LoadConfig(apiConfig.Config{})
+	mongo, err := database.ConnectToMongoDB(conf.MongoDBURI, conf.MongoDBName)
+	if err != nil {
+		panic(err)
+	}
+
+	client := "conure"
+	email := "admin@conure.io"
+	password := GenerateRandomPassword(10)
+	hashedPassword, err := GenerateFromPassword(password)
+	if err != nil {
+		panic(err)
+	}
+
+	user := User{
+		Email:    email,
+		Password: hashedPassword,
+		Client:   client,
+	}
+	err = user.GetByEmail(mongo, email)
+	if err != nil {
+		panic(err)
+	}
+	err = user.UpdatePassword(mongo, hashedPassword)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x")
+	log.Println("Superuser password reset")
 	log.Println("Email:", email)
 	log.Println("Password:", password)
 	log.Println("x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x")
