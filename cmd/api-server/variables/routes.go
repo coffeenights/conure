92607dd33f3c0d0fe9ea3d3863d727ca58d13bc5
Oscar Lotero <oscarelotero@gmail.com>
2024-03-08 15:18:50 +0100

    Update middleware in routes.go

--- cmd/api-server/variables/routes.go
+++ cmd/api-server/variables/routes.go
@@ -8,11 +8,12 @@ import (
 func GenerateRoutes(relativePath string, r *gin.Engine, handler *Handler) {
 	paths := r.Group(relativePath)
 	{
-		paths.POST("/:organizationID", middlewares.CheckAuthenticatedUser(handler.Config), handler.CreateVariable)
-		paths.GET("/:organizationID", middlewares.CheckAuthenticatedUser(handler.Config), handler.ListOrganizationVariables)
-		paths.POST("/:organizationID/:applicationID/e/:environmentID", middlewares.CheckAuthenticatedUser(handler.Config), handler.CreateVariable)
-		paths.GET("/:organizationID/:applicationID/e/:environmentID", middlewares.CheckAuthenticatedUser(handler.Config), handler.ListEnvironmentVariables)
-		paths.POST("/:organizationID/:applicationID/e/:environmentID/c/:componentID", middlewares.CheckAuthenticatedUser(handler.Config), handler.CreateVariable)
-		paths.GET("/:organizationID/:applicationID/e/:environmentID/c/:componentID", middlewares.CheckAuthenticatedUser(handler.Config), handler.ListComponentVariables)
+		paths.POST("/:organizationID", middlewares.CheckAuthenticatedUser(handler.Config, handler.MongoDB),
+			handler.CreateVariable)
+		paths.GET("/:organizationID", middlewares.CheckAuthenticatedUser(handler.Config, handler.MongoDB), handler.ListOrganizationVariables)
+		paths.POST("/:organizationID/:applicationID/e/:environmentID", middlewares.CheckAuthenticatedUser(handler.Config, handler.MongoDB), handler.CreateVariable)
+		paths.GET("/:organizationID/:applicationID/e/:environmentID", middlewares.CheckAuthenticatedUser(handler.Config, handler.MongoDB), handler.ListEnvironmentVariables)
+		paths.POST("/:organizationID/:applicationID/e/:environmentID/c/:componentID", middlewares.CheckAuthenticatedUser(handler.Config, handler.MongoDB), handler.CreateVariable)
+		paths.GET("/:organizationID/:applicationID/e/:environmentID/c/:componentID", middlewares.CheckAuthenticatedUser(handler.Config, handler.MongoDB), handler.ListComponentVariables)
 	}
 }
