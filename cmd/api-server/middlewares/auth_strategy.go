d2537bb2840132b643abb7f64df7241c60cd6f6c
Oscar Lotero <oscarelotero@gmail.com>
2024-03-08 15:19:38 +0100

    Refactor authentication strategies in API server middleware

--- cmd/api-server/middlewares/auth_strategy.go
+++ cmd/api-server/middlewares/auth_strategy.go
@@ -1,1 +1,25 @@
 package middlewares
+
+import (
+	"github.com/coffeenights/conure/cmd/api-server/auth"
+	apiConfig "github.com/coffeenights/conure/cmd/api-server/config"
+	"github.com/coffeenights/conure/cmd/api-server/database"
+)
+
+type AuthStrategy interface {
+	ValidateUser(token string, config *apiConfig.Config, mongo *database.MongoDB) (auth.User, error)
+}
+
+var strategies = map[string]AuthStrategy{
+	"local":    &LocalAuthStrategy{},
+	"external": &ExternalAuthStrategy{},
+}
+
+func ValidateUser(token string, config *apiConfig.Config, mongo *database.MongoDB) (auth.User, error) {
+	authSystem := config.AuthStrategySystem
+	strategy, ok := strategies[authSystem]
+	if !ok {
+		return auth.User{}, ErrUnsupportedStrategy
+	}
+	return strategy.ValidateUser(token, config, mongo)
+}
--- cmd/api-server/middlewares/external_strategy.go
+++ cmd/api-server/middlewares/external_strategy.go
@@ -1,1 +1,46 @@
 package middlewares
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"github.com/coffeenights/conure/cmd/api-server/auth"
+	apiConfig "github.com/coffeenights/conure/cmd/api-server/config"
+	"github.com/coffeenights/conure/cmd/api-server/database"
+)
+
+type ExternalAuthStrategy struct{}
+
+func (e *ExternalAuthStrategy) ValidateUser(token string, config *apiConfig.Config, _ *database.MongoDB) (auth.User,
+	error) {
+	user := auth.User{}
+	req, err := http.NewRequest("GET", config.AuthServiceURL, nil)
+	if err != nil {
+		return user, auth.ErrUnauthorized
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return user, auth.ErrUnauthorized
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return user, auth.ErrUnauthorized
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return user, auth.ErrUnauthorized
+	}
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		return user, auth.ErrUnauthorized
+	}
+
+	return user, nil
+}
