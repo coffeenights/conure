c52501bf140135d82df0ca9aa6f5c05dedbb0318
mredvard <1638026+mredvard@users.noreply.github.com>
2024-03-15 13:16:37 +0800

    test: Add the tests for DetailApplications

--- cmd/api-server/applications/applications_test.go
+++ cmd/api-server/applications/applications_test.go
@@ -74,3 +74,39 @@ func TestListApplications_Empty(t *testing.T) {
 	}
 	_ = org.Delete(app.MongoDB)
 }
+
+func TestDetailApplication(t *testing.T) {
+	router, app := setupRouter()
+	// Create test organization
+	org := Organization{
+		Status:    OrgActive,
+		AccountID: "testOrgId",
+		Name:      "Test Organization for DetailApplication",
+	}
+	oID, err := org.Create(app.MongoDB) // lint:ignore
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create test application
+	app1, err := NewApplication(oID, "TestDetailApplication", primitive.NewObjectID().Hex()).Create(app.MongoDB)
+	if err != nil {
+		t.Fatal(err)
+	}
+	envName := "test-detail-application"
+	_, err = app1.CreateEnvironment(app.MongoDB, envName)
+	if err != nil {
+		t.Fatal(err)
+	}
+	url := "/organizations/" + oID + "/a/" + app1.ID.Hex() + "/e/" + envName + "/"
+	req, _ := http.NewRequest("GET", url, nil)
+	resp := httptest.NewRecorder()
+	router.ServeHTTP(resp, req)
+
+	// Assert
+	if resp.Code != http.StatusOK {
+		t.Errorf("Expected response code 200, got: %v", resp.Code)
+	}
+	_ = app1.Delete(app.MongoDB)
+	_ = org.Delete(app.MongoDB)
+}
