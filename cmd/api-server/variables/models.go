5381691d46bf9d8ac544cc7fe232e257150ab5d0
Oscar Lotero <oscarelotero@gmail.com>
2024-03-15 12:35:15 +0100

    Update Variable struct and methods to use the primitive.ObjectID

--- cmd/api-server/variables/models.go
+++ cmd/api-server/variables/models.go
@@ -15,18 +15,18 @@ import (
 const VariableCollection string = "variables"
 
 type Variable struct {
-	ID             primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	Client         string             `bson:"client" json:"client"`
-	Name           string             `bson:"name" json:"name" binding:"required"`
-	Value          string             `bson:"value" json:"value" binding:"required"`
-	Type           VariableType       `bson:"type" json:"type"`
-	OrganizationID string             `bson:"organizationId,omitempty" json:"organization_id,omitempty"`
-	ApplicationID  *string            `bson:"applicationId,omitempty" json:"application_id,omitempty"`
-	EnvironmentID  *string            `bson:"environmentId,omitempty" json:"environment_id,omitempty"`
-	ComponentID    *string            `bson:"componentId,omitempty" json:"component_id,omitempty"`
-	IsEncrypted    bool               `bson:"isEncrypted" json:"is_encrypted"`
-	CreatedAt      time.Time          `bson:"createdAt" json:"created_at"`
-	UpdatedAt      time.Time          `bson:"updatedAt" json:"updated_at"`
+	ID             primitive.ObjectID  `bson:"_id,omitempty" json:"id"`
+	Client         string              `bson:"client" json:"client"`
+	Name           string              `bson:"name" json:"name" binding:"required"`
+	Value          string              `bson:"value" json:"value" binding:"required"`
+	Type           VariableType        `bson:"type" json:"type"`
+	OrganizationID primitive.ObjectID  `bson:"organizationId,omitempty" json:"organization_id,omitempty"`
+	ApplicationID  *primitive.ObjectID `bson:"applicationId,omitempty" json:"application_id,omitempty"`
+	EnvironmentID  *string             `bson:"environmentId,omitempty" json:"environment_id,omitempty"`
+	ComponentID    *primitive.ObjectID `bson:"componentId,omitempty" json:"component_id,omitempty"`
+	IsEncrypted    bool                `bson:"isEncrypted" json:"is_encrypted"`
+	CreatedAt      time.Time           `bson:"createdAt" json:"created_at"`
+	UpdatedAt      time.Time           `bson:"updatedAt" json:"updated_at"`
 }
 
 func (v *Variable) ValidateName() bool {
@@ -75,7 +75,7 @@ func (v *Variable) Delete(mongo *database.MongoDB) error {
 	return nil
 }
 
-func (v *Variable) ListByOrg(mongo *database.MongoDB, client, organizationID string) ([]Variable, error) {
+func (v *Variable) ListByOrg(mongo *database.MongoDB, client string, organizationID primitive.ObjectID) ([]Variable, error) {
 	collection := mongo.Client.Database(mongo.DBName).Collection(VariableCollection)
 	findOptions := options.Find()
 	findOptions.SetSort(bson.D{{"name", 1}})
@@ -92,8 +92,7 @@ func (v *Variable) ListByOrg(mongo *database.MongoDB, client, organizationID str
 	return variables, nil
 }
 
-func (v *Variable) ListByEnv(mongo *database.MongoDB, client, organizationID, applicationID,
-	environmentID string) ([]Variable, error) {
+func (v *Variable) ListByEnv(mongo *database.MongoDB, client string, organizationID, applicationID primitive.ObjectID, environmentID string) ([]Variable, error) {
 	collection := mongo.Client.Database(mongo.DBName).Collection(VariableCollection)
 	findOptions := options.Find()
 	findOptions.SetSort(bson.D{{"name", 1}})
@@ -111,8 +110,7 @@ func (v *Variable) ListByEnv(mongo *database.MongoDB, client, organizationID, ap
 	return variables, nil
 }
 
-func (v *Variable) ListByComp(mongo *database.MongoDB, client, organizationID, applicationID,
-	environmentID, componentID string) ([]Variable, error) {
+func (v *Variable) ListByComp(mongo *database.MongoDB, client string, organizationID, applicationID primitive.ObjectID, environmentID string, componentID primitive.ObjectID) ([]Variable, error) {
 	collection := mongo.Client.Database(mongo.DBName).Collection(VariableCollection)
 	findOptions := options.Find()
 	findOptions.SetSort(bson.D{{"name", 1}})
@@ -130,7 +128,7 @@ func (v *Variable) ListByComp(mongo *database.MongoDB, client, organizationID, a
 	return variables, nil
 }
 
-func (v *Variable) GetByOrgAndName(mongo *database.MongoDB, client string, organizationID string, name string) error {
+func (v *Variable) GetByOrgAndName(mongo *database.MongoDB, client string, organizationID primitive.ObjectID, name string) error {
 	collection := mongo.Client.Database(mongo.DBName).Collection(VariableCollection)
 	err := collection.FindOne(context.Background(), primitive.M{"client": client,
 		"organizationId": organizationID, "name": name}).Decode(v)
@@ -140,8 +138,7 @@ func (v *Variable) GetByOrgAndName(mongo *database.MongoDB, client string, organ
 	return nil
 }
 
-func (v *Variable) GetByAppIDAndEnvAndName(mongo *database.MongoDB, client string, applicationID string, t VariableType,
-	environmentID *string, name string) error {
+func (v *Variable) GetByAppIDAndEnvAndName(mongo *database.MongoDB, client string, applicationID primitive.ObjectID, t VariableType, environmentID *string, name string) error {
 	collection := mongo.Client.Database(mongo.DBName).Collection(VariableCollection)
 	err := collection.FindOne(context.Background(), primitive.M{"client": client, "applicationId": applicationID,
 		"type": t, "name": name, "environmentId": environmentID}).Decode(v)
@@ -151,8 +148,7 @@ func (v *Variable) GetByAppIDAndEnvAndName(mongo *database.MongoDB, client strin
 	return nil
 }
 
-func (v *Variable) GetByAppIDAndEnvAndCompAndName(mongo *database.MongoDB, client string, applicationID string, t VariableType,
-	environmentID *string, componentID *string, name string) error {
+func (v *Variable) GetByAppIDAndEnvAndCompAndName(mongo *database.MongoDB, client string, applicationID primitive.ObjectID, t VariableType, environmentID *string, componentID *primitive.ObjectID, name string) error {
 	collection := mongo.Client.Database(mongo.DBName).Collection(VariableCollection)
 	err := collection.FindOne(context.Background(), primitive.M{"client": client, "applicationId": applicationID,
 		"type": t, "name": name, "environmentId": environmentID, "componentId": componentID}).Decode(v)
