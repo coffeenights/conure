706bac19bb71d62fcd739c22e208be57da070704
Edgar Contreras <1638026+mredvard@users.noreply.github.com>
2024-03-19 10:39:19 +0800

    feat: Work on the component status module

--- cmd/api-server/applications/components.go
+++ cmd/api-server/applications/components.go
@@ -1,10 +1,8 @@
 package applications
 
 import (
-	"errors"
 	k8sUtils "github.com/coffeenights/conure/internal/k8s"
 	"github.com/gin-gonic/gin"
-	"github.com/oam-dev/kubevela-core-api/apis/core.oam.dev/common"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"log"
 	"net/http"
@@ -32,99 +30,39 @@ func (a *ApiHandler) ListComponents(c *gin.Context) {
 	response.Components = make([]ComponentResponse, len(components))
 	for i, component := range components {
 		response.Components[i] = ComponentResponse{
-			&component,
+			Component: &component,
 		}
 	}
 	c.JSON(http.StatusOK, response)
 }
 
 func (a *ApiHandler) DetailComponent(c *gin.Context) {
-	clientset, err := k8sUtils.GetClientset()
+	handler, err := NewApplicationHandler(a.MongoDB)
 	if err != nil {
-		log.Printf("Error getting clientset: %v\n", err)
+		log.Printf("Error creating application handler: %v\n", err)
 		c.AbortWithStatus(http.StatusInternalServerError)
 		return
 	}
-
-	application := NewApplication(c.Param("organizationID"), "", "")
-	namespace := application.GetNamespace()
-
-	labels := map[string]string{
-		"conure.io/organization-id": c.Param("organizationID"),
-		"conure.io/application-id":  c.Param("applicationID"),
-		"conure.io/environment":     c.Param("environment"),
-	}
-
-	applicationDef, err := k8sUtils.GetApplicationByLabels(clientset, namespace, labels)
+	_, err = handler.Model.GetByID(a.MongoDB, c.Param("applicationID"))
 	if err != nil {
-		switch {
-		case errors.Is(err, k8sUtils.ErrApplicationNotFound):
-			c.AbortWithStatus(http.StatusNotFound)
-			return
-		default:
-			log.Printf("Error getting application: %v\n", err)
-			c.AbortWithStatus(http.StatusInternalServerError)
-			return
-		}
-	}
-
-	// Extract the component
-	var componentSpec common.ApplicationComponent
-	for _, comp := range applicationDef.Spec.Components {
-		if comp.Name == c.Param("componentName") {
-			componentSpec = comp
-			break
-		}
-	}
-	if componentSpec.Name == "" {
 		c.AbortWithStatus(http.StatusNotFound)
 		return
 	}
 
-	// Extract the component Status
-	var componentStatus common.ApplicationComponentStatus
-	for _, comp := range applicationDef.Status.Services {
-		if comp.Name == c.Param("componentName") {
-			componentStatus = comp
-			break
-		}
-	}
-
-	if componentStatus.Name == "" {
-		c.AbortWithStatus(http.StatusNotFound)
+	components, err := handler.Model.ListComponents(a.MongoDB)
+	if err != nil {
+		log.Printf("Error getting components: %v\n", err)
+		c.AbortWithStatus(http.StatusInternalServerError)
 		return
 	}
-
-	componentProperties := ComponentProperties{
-		Name: componentSpec.Name,
-		Type: componentSpec.Type,
+	var response ComponentListResponse
+	response.Components = make([]ComponentResponse, len(components))
+	for i, component := range components {
+		response.Components[i] = ComponentResponse{
+			Component: &component,
+		}
 	}
-	wl, err := NewK8sWorkload(application, &componentSpec, &componentStatus)
-	if err != nil {
-		log.Printf("Error creating workload: %v\n", err)
-	}
-	componentProperties.NetworkProperties, err = wl.GetNetworkProperties()
-	if err != nil {
-		log.Printf("Error extracting network properties: %v\n", err)
-	}
-	componentProperties.ResourcesProperties, err = wl.GetResourcesProperties()
-	if err != nil {
-		log.Printf("Error extracting resources properties: %v\n", err)
-	}
-
-	componentProperties.StorageProperties, err = wl.GetStorageProperties()
-	if err != nil {
-		log.Printf("Error extracting storage properties: %v\n", err)
-	}
-
-	componentProperties.SourceProperties, err = wl.GetSourceProperties()
-	if err != nil {
-		log.Printf("Error extracting source properties: %v\n", err)
-	}
-
-	var componentResponse ServiceComponentResponse
-	componentResponse.ComponentProperties = componentProperties
-	c.JSON(http.StatusOK, componentResponse)
+	c.JSON(http.StatusOK, response)
 }
 
 func (a *ApiHandler) StatusComponent(c *gin.Context) {
