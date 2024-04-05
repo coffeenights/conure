31ca9ceaa983ab5c954fa55f2d21e6f97943bbe4
mredvard <1638026+mredvard@users.noreply.github.com>
2024-04-05 11:12:42 +0800

    feat: Allow to choose the host for the server

--- cmd/api-server/main.go
+++ cmd/api-server/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	apiConfig "github.com/coffeenights/conure/cmd/api-server/config"
 	"github.com/coffeenights/conure/cmd/api-server/routes"
+	"github.com/coffeenights/conure/internal/config"
 	_ "github.com/joho/godotenv/autoload"
 	"log"
 )
 
 func main() {
 	r := routes.GenerateRouter()
-	err := r.Run(":8080")
+	conf := config.LoadConfig(apiConfig.Config{})
+
+	err := r.Run(conf.APIHost + ":8080")
 	if err != nil {
 		log.Fatal(err)
 	}
