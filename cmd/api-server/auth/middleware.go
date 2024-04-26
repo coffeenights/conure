0c42ce688660e303a6e251e92dbc77e7ffc439ad
Oscar Lotero <oscarelotero@gmail.com>
2024-04-26 13:09:49 +0200

    Auth - Use the new method for error handling

--- cmd/api-server/auth/middleware.go
+++ cmd/api-server/auth/middleware.go
@@ -1,37 +1,30 @@
 package auth
 
 import (
-	"github.com/coffeenights/conure/cmd/api-server/models"
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 
 	apiConfig "github.com/coffeenights/conure/cmd/api-server/config"
+	"github.com/coffeenights/conure/cmd/api-server/conureerrors"
 	"github.com/coffeenights/conure/cmd/api-server/database"
+	"github.com/coffeenights/conure/cmd/api-server/models"
 )
 
 func CheckCurrentUser(config *apiConfig.Config, mongo *database.MongoDB) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		authToken, err := c.Cookie("auth")
 		if err != nil {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-				"error": ErrUnauthorized.Error(),
-			})
+			conureerrors.AbortWithError(c, conureerrors.ErrUnauthorized)
 			return
 		}
 		if authToken == "" {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-				"error": ErrUnauthorized.Error(),
-			})
+			conureerrors.AbortWithError(c, conureerrors.ErrUnauthorized)
 			return
 		}
 
 		// validate the token signature and retrieve the jwt payload
 		claims, err := ValidateToken(authToken, config.JWTSecret)
 		if err != nil {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-				"error": ErrUnauthorized.Error(),
-			})
+			conureerrors.AbortWithError(c, err)
 			return
 		}
 
@@ -39,9 +32,7 @@ func CheckCurrentUser(config *apiConfig.Config, mongo *database.MongoDB) gin.Han
 		user := models.User{}
 		err = user.GetByEmail(mongo, claims.Data.Email)
 		if err != nil {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-				"error": ErrUnauthorized.Error(),
-			})
+			conureerrors.AbortWithError(c, conureerrors.ErrUnauthorized)
 			return
 		}
 		c.Set("currentUser", user)
