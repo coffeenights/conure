7493a595bc10f60d62ecf9ed34e36aff11e8761c
mredvard <1638026+mredvard@users.noreply.github.com>
2024-07-02 13:11:20 +0800

    tests: Create a helper function to get a Component template

--- cmd/api-server/models/test_utils.go
+++ cmd/api-server/models/test_utils.go
@@ -4,6 +4,7 @@ import (
 	apiConfig "github.com/coffeenights/conure/cmd/api-server/config"
 	"github.com/coffeenights/conure/cmd/api-server/database"
 	"github.com/coffeenights/conure/internal/config"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func SetupDB() (*database.MongoDB, error) {
@@ -14,4 +15,41 @@ func SetupDB() (*database.MongoDB, error) {
 		return nil, err
 	}
 	return &database.MongoDB{Client: client.Client, DBName: testDBName}, nil
+}
+
+func ComponentTemplate(appID primitive.ObjectID, name string) *Component {
+	return &Component{
+		ApplicationID: appID,
+		Name:          name,
+		Type:          "service",
+		Settings: ComponentSettings{
+			ResourcesSettings: ResourcesSettings{
+				Replicas: 1,
+				CPU:      0.5,
+				Memory:   200,
+			},
+			SourceSettings: SourceSettings{
+				Repository: "coffeenights/django:latest",
+				Command:    "python manage.py runserver 0.0.0.0:8000",
+			},
+			NetworkSettings: NetworkSettings{
+				Exposed: true,
+				Type:    "public",
+				Ports: []PortSettings{
+					{
+						HostPort:   8000,
+						TargetPort: 8000,
+						Protocol:   "tcp",
+					},
+				},
+			},
+			StorageSettings: []StorageSettings{
+				{
+					Size:      20,
+					Name:      "Volume1",
+					MountPath: "/tmp",
+				},
+			},
+		},
+	}
 }
