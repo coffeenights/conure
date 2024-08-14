b601104e806027b9bcd7b4c564126d9bd41d9576
mredvard <1638026+mredvard@users.noreply.github.com>
2024-08-14 12:01:01 +0800

    feat: Remove the previous  controller logic and start working towards building the manifest using timoni

--- internal/controller/core/handler.go
+++ internal/controller/core/handler.go
@@ -3,7 +3,7 @@ package controller
 import (
 	"context"
 	coreconureiov1alpha1 "github.com/coffeenights/conure/apis/core/v1alpha1"
-	"github.com/coffeenights/conure/internal/workload"
+	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -19,44 +19,16 @@ type ApplicationHandler struct {
 	Application *coreconureiov1alpha1.Application
 	Reconciler  *ApplicationReconciler
 	Ctx         context.Context
+	Logger      logr.Logger
 	Workloads   []Workload
 }
 
 func NewApplicationHandler(ctx context.Context, application *coreconureiov1alpha1.Application, reconciler *ApplicationReconciler) (*ApplicationHandler, error) {
-	logger := log.FromContext(ctx)
 	var handler ApplicationHandler
+	handler.Logger = log.FromContext(ctx)
 	handler.Application = application
 	handler.Ctx = ctx
 	handler.Reconciler = reconciler
-	for _, component := range application.Spec.Components {
-		switch component.Type {
-		case coreconureiov1alpha1.Service:
-			componentProperties := coreconureiov1alpha1.ServiceComponentProperties{}
-			err := componentProperties.Parse(component.Properties.Raw)
-			if err != nil {
-				return &handler, err
-			}
-			wld := workload.ServiceWorkload{
-				Application: application,
-				Component:   &component,
-				Properties:  &componentProperties,
-			}
-			err = wld.Build()
-			if err != nil {
-				logger.Error(err, "unable to construct workload from Application template")
-				return &handler, err
-			}
-			err = wld.SetControllerReference(reconciler.Scheme)
-			if err != nil {
-				logger.Error(err, "unable to set the reference back to the controller in the workload")
-				return &handler, err
-			}
-			handler.Workloads = append(handler.Workloads, &wld)
-		case coreconureiov1alpha1.StatefulService:
-		case coreconureiov1alpha1.CronTask:
-		case coreconureiov1alpha1.Worker:
-		}
-	}
 	return &handler, nil
 }
 
