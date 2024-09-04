dc0a9bf67387538f1e645c5e958b85cc3d8ec856
mredvard <1638026+mredvard@users.noreply.github.com>
2024-09-04 15:11:59 +0800

    feat: Disable temporarily the component controller

--- internal/controller/core/setup.go
+++ internal/controller/core/setup.go
@@ -2,7 +2,7 @@ package controller
 
 import (
 	"github.com/coffeenights/conure/internal/controller/core/application"
-	"github.com/coffeenights/conure/internal/controller/core/component"
+	"github.com/coffeenights/conure/internal/controller/core/workflow"
 	ctrl "sigs.k8s.io/controller-runtime"
 )
 
@@ -10,8 +10,8 @@ func Setup(mgr ctrl.Manager) error {
 	// Define a slice of setup functions
 	setupFunctions := []func(ctrl.Manager) error{
 		application.Setup,
-		// workflow.Setup,
-		component.Setup,
+		workflow.Setup,
+		// component.Setup,
 	}
 	// Iterate over each setup function
 	for _, setup := range setupFunctions {
