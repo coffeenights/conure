1701db2038f6ee5a2725bfc53b799f1b47e91e78
mredvard <1638026+mredvard@users.noreply.github.com>
2024-09-04 15:12:49 +0800

    feat: Manage condition status of the workflowrun

--- internal/controller/core/workflow/workflow_controller.go
+++ internal/controller/core/workflow/workflow_controller.go
@@ -2,8 +2,10 @@ package workflow
 
 import (
 	"context"
-	coreconureiov1alpha1 "github.com/coffeenights/conure/apis/core/v1alpha1"
+	conurev1alpha1 "github.com/coffeenights/conure/apis/core/v1alpha1"
+	"github.com/coffeenights/conure/internal/controller/core/common"
 	batchv1 "k8s.io/api/batch/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -20,12 +22,12 @@ type WorkflowReconciler struct {
 func (r *WorkflowReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	logger := log.FromContext(ctx)
 
-	var wflr coreconureiov1alpha1.WorkflowRun
+	var wflr conurev1alpha1.WorkflowRun
 	if err := r.Get(ctx, req.NamespacedName, &wflr); err != nil {
 		logger.Info("WorkflowRun resource not found.")
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
-	var app coreconureiov1alpha1.Application
+	var app conurev1alpha1.Application
 	nsn := types.NamespacedName{
 		Namespace: req.Namespace,
 		Name:      wflr.Spec.ApplicationName,
@@ -35,7 +37,7 @@ func (r *WorkflowReconciler) Reconcile(ctx context.Context, req ctrl.Request) (c
 		logger.Info("Application resource not found.")
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
-	var wflw coreconureiov1alpha1.Workflow
+	var wflw conurev1alpha1.Workflow
 	nsn = types.NamespacedName{
 		Namespace: req.Namespace,
 		Name:      wflr.Spec.WorkflowName,
@@ -46,8 +48,8 @@ func (r *WorkflowReconciler) Reconcile(ctx context.Context, req ctrl.Request) (c
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
-	if !wflr.Status.Finished {
-		actionsHandler, err := NewActionsHandler(ctx, r.Client, wflr.Namespace, &wflw)
+	if !r.isFinished(&wflr) {
+		actionsHandler, err := NewActionsHandler(ctx, wflr.Namespace, &wflw, r)
 		if err != nil {
 			return ctrl.Result{}, err
 		}
@@ -55,20 +57,49 @@ func (r *WorkflowReconciler) Reconcile(ctx context.Context, req ctrl.Request) (c
 		if err != nil {
 			return ctrl.Result{}, err
 		}
-		err = actionsHandler.RunActions()
+		err = r.setCondition(ctx, &wflr, conurev1alpha1.ConditionTypeRunningAction, conurev1alpha1.RunningActionReason, "Running actions")
 		if err != nil {
 			return ctrl.Result{}, err
 		}
-		wflr.Status.Finished = true
+		err = actionsHandler.RunActions()
+		if err != nil {
+			condErr := r.setCondition(ctx, &wflr, conurev1alpha1.ConditionTypeFinished, conurev1alpha1.FinishedFailedReason, "Failed to run actions")
+			if condErr != nil {
+				logger.Error(condErr, "Failed to set condition")
+			}
+			return ctrl.Result{}, err
+		}
+		err = r.setCondition(ctx, &wflr, conurev1alpha1.ConditionTypeFinished, conurev1alpha1.FinishedSuccesfullyReason, "Finished")
+		if err != nil {
+			return ctrl.Result{}, err
+		}
 	}
 
 	return ctrl.Result{}, nil
 }
 
+func (r *WorkflowReconciler) setCondition(ctx context.Context, wflr *conurev1alpha1.WorkflowRun, conditionType conurev1alpha1.WorkflowConditionType, reason conurev1alpha1.WorkflowConditionReason, message string) error {
+	var newConditions []metav1.Condition
+	wflr.Status.Conditions = common.SetCondition(newConditions, string(conditionType), metav1.ConditionTrue, string(reason), message)
+	err := r.Status().Update(ctx, wflr)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *WorkflowReconciler) isFinished(wflr *conurev1alpha1.WorkflowRun) bool {
+	index, exists := common.ContainsCondition(wflr.Status.Conditions, conurev1alpha1.ConditionTypeFinished.String())
+	if exists && wflr.Status.Conditions[index].Reason == conurev1alpha1.FinishedSuccesfullyReason.String() {
+		return true
+	}
+	return false
+}
+
 // SetupWithManager sets up the controller with the Manager.
 func (r *WorkflowReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&coreconureiov1alpha1.WorkflowRun{}).
+		For(&conurev1alpha1.WorkflowRun{}).
 		Owns(&batchv1.Job{}).
 		Complete(r)
 }
