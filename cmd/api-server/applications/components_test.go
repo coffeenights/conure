5ffbec9144ed39a3304d3ea26cea7d91adc0cca7
mredvard <1638026+mredvard@users.noreply.github.com>
2026-04-04 14:30:04 +0800

    tests: Update CPU resource setting type from float to string in components test

--- cmd/api-server/applications/components_test.go
+++ cmd/api-server/applications/components_test.go
@@ -338,7 +338,7 @@ func TestUpdateComponent(t *testing.T) {
 	defer comp.Delete(testConf.app.MongoDB)
 
 	// Modify a  property
-	comp.Settings.ResourcesSettings.CPU = 1.0
+	comp.Settings.ResourcesSettings.CPU = "1000m"
 	payload, err := json.Marshal(comp)
 
 	url := "/organizations/" + oID + "/a/" + application.ID.Hex() + "/e/" + env.Name + "/c/" + comp.ID.Hex()
@@ -352,8 +352,8 @@ func TestUpdateComponent(t *testing.T) {
 		t.Errorf("Expected response code 200, got: %v", resp.Code)
 	}
 
-	if comp.Settings.ResourcesSettings.CPU != 1.0 {
-		t.Errorf("Expected CPU to be 1.0, got: %v", comp.Settings.ResourcesSettings.CPU)
+	if comp.Settings.ResourcesSettings.CPU != "1000m" {
+		t.Errorf("Expected CPU to be 1000m, got: %v", comp.Settings.ResourcesSettings.CPU)
 	}
 
 }
